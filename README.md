9f7975da633017ad15e454e068cf446a06a25f6e
Oscar Wu <tabowsy@gmail.com>
2022-01-05 15:22:51 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Deep Reinforcement Learning to Indoor Unmanned Vehicle Navigation
+I used Gmapping SLAM to build my house environment, and trained the Turtlebot Waffle as an unmanned vehicle with DQN and DDPG.
+This project is for academic use which modified official code and also developed extra functions.
+## Reference and Related Packages
+- https://github.com/ROBOTIS-GIT/turtlebot3
+- https://github.com/ROBOTIS-GIT/turtlebot3_msgs
+- https://github.com/ROBOTIS-GIT/turtlebot3_simulations
+- https://github.com/ROBOTIS-GIT/turtlebot3_machine_learning
---
+++ src/respawnGoal_house.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+#################################################################################
+# Copyright 2018 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#################################################################################
+
+# Authors: Gilbert #
+
+import rospy
+import random
+import time
+import os
+from gazebo_msgs.srv import SpawnModel, DeleteModel
+from gazebo_msgs.msg import ModelStates
+from geometry_msgs.msg import Pose
+
+class Respawn():
+    def __init__(self):
+        self.modelPath = os.path.dirname(os.path.realpath(__file__))
+        self.modelPath = self.modelPath.replace('IV_project/src',
+                                                'turtlebot3_simulations/turtlebot3_gazebo/models/turtlebot3_square/goal_box/model.sdf')
+        self.f = open(self.modelPath, 'r')
+        self.model = self.f.read()
+        self.goal_position = Pose()
+        self.init_goal_x = 0.6
+        self.init_goal_y = 0.0
+        self.goal_position.position.x = self.init_goal_x
+        self.goal_position.position.y = self.init_goal_y
+        self.modelName = 'goal'
+        self.obstacle_1 = 0.6, 0.6
+        self.obstacle_2 = 0.6, -0.6
+        self.obstacle_3 = -0.6, 0.6
+        self.obstacle_4 = -0.6, -0.6
+        self.last_goal_x = self.init_goal_x
+        self.last_goal_y = self.init_goal_y
+        self.last_index = 0
+        self.sub_model = rospy.Subscriber('gazebo/model_states', ModelStates, self.checkModel)
+        self.check_model = False
+        self.index = 0
+
+    def checkModel(self, model):
+        self.check_model = False
+        for i in range(len(model.name)):
+            if model.name[i] == "goal":
+                self.check_model = True
+
+    def respawnModel(self):
+        while True:
+            if not self.check_model:
+                rospy.wait_for_service('gazebo/spawn_sdf_model')
+                spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_sdf_model', SpawnModel)
+                spawn_model_prox(self.modelName, self.model, 'robotos_name_space', self.goal_position, "world")
+                rospy.loginfo("Goal position : %.1f, %.1f", self.goal_position.position.x,
+                              self.goal_position.position.y)
+                break
+            else:
+                pass
+
+    def deleteModel(self):
+        while True:
+            if self.check_model:
+                rospy.wait_for_service('gazebo/delete_model')
+                del_model_prox = rospy.ServiceProxy('gazebo/delete_model', DeleteModel)
+                del_model_prox(self.modelName)
+                break
+            else:
+                pass
+
+    def getPosition(self, position_check=False, delete=False):
+        if delete:
+            self.deleteModel()
+
+        while position_check:
+            goal_x_list = [0.5,-0.5,-1,0.5,1.35,1.8,2.4,3,3.3,4.2,5.5,5.5,6.2,6.85,6.85,6.4,4.9,8.43,8.3,8.3,4.9,5.9,6.7]
+            goal_y_list = [1,0,0,1.8,1.8,1.8,1.85,1.8,1.8,1.7,1.8,1,0.65,2.3,3.4,4.6,4.9,4.67,2.2,0.74,-0.57,-1.4,1.15]
+
+            self.index = random.randrange(0, len(goal_x_list))
+            print(self.index, self.last_index)
+            if self.last_index == self.index:
+                position_check = True
+            else:
+                self.last_index = self.index
+                position_check = False
+
+            self.goal_position.position.x = goal_x_list[self.index]
+            self.goal_position.position.y = goal_y_list[self.index]
+
+        time.sleep(0.5)
+        self.respawnModel()
+
+        self.last_goal_x = self.goal_position.position.x
+        self.last_goal_y = self.goal_position.position.y
+
+        return self.goal_position.position.x, self.goal_position.position.y
